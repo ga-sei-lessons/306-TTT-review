878ac813d73319740b5f63806db6d11ea4f478d7
weston-bailey <wdbailey87@gmail.com>
2023-03-13 10:30:56 -0700

    adds html and css skeleton

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+console.log("hello, TTT!")
+// APP STATE (varaibles)
+    // players choices (a vritual representation of the gameboard)
+        // use an array (1d array approach) ['x', '', '', '', '', '', '', 'o', '']
+        // 2d array approach
+        // [
+        //     ['x', '', ''],
+        //     ['', '', ''],
+        //     ['', 'o', '']
+        // ]
+    // whose turn it is (is it x or o)
+    // a boolean value that tells you if the game is currently running
+    // (optional) a turn counter that is used to detect cats games when it reaches 9
+    // (bonus) -- how many times each player has won
+
+// EVENT LISTENERS
+    // a click on the reset button
+    // event listener for when you click on the gameboard
+        // when a square is clicked we should do the following:
+        // set the innertext of the square that got clicked on of whose turn it currently it
+        // prevent repeat clicks -- check the gameboard array or check the innertext
+        // set the move into the gameboard array 
+        // check if there is a winner
+            // if someone has won, end the game and updated the message for who has won
+                // think about all the conditions to win the game and check for all of them
+            // check for cats game
+                // if every square is an x or an o and no one has won   
+                // or count turns    
+            // if no one has won, let gameplay continue (switch turns)
